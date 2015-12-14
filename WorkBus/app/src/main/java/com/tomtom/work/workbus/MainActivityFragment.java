2c031c16be4e651997fc0b968551b6e2cd74dde1
mariopce <mariopce@gmail.com>
2015-12-14 22:52:48 +0100

    [TRAVIS] change build tool to 24.4.1

--- WorkBus/app/src/main/java/com/tomtom/work/workbus/MainActivityFragment.java
+++ WorkBus/app/src/main/java/com/tomtom/work/workbus/MainActivityFragment.java
@@ -1,15 +1,20 @@
 package com.tomtom.work.workbus;
 
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private Button agrafButton;
 
     public MainActivityFragment() {
     }
@@ -17,6 +22,21 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        agrafButton =  (Button)rootView.findViewById(R.id.agraf_button);
+        agrafButton.setOnClickListener(new ShowToastClickListener("Clicked on Agraf Button"));
+        return rootView;
+    }
+    private static class ShowToastClickListener implements View.OnClickListener{
+        private String message;
+
+        public ShowToastClickListener(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(v.getContext(), message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
