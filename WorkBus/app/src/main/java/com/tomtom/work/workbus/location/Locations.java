23abfe6ff423bc91ba804bf89c8d3b17f90ebbb5
mariopce <mariopce@gmail.com>
2015-12-15 22:05:04 +0100

    Define locations

---
+++ WorkBus/app/src/main/java/com/tomtom/work/workbus/location/Locations.java
@@ -0,0 +1,32 @@
+package com.tomtom.work.workbus.location;
+
+import android.location.Location;
+
+/**
+ * Created by mario on 15.12.15.
+ */
+public class Locations {
+    //Util class
+    private Locations() {
+    }
+
+    public Location getOrionOfficeLocation(){
+        Location l = new Location("orion");
+        l.setLatitude(51.7604829);
+        l.setLongitude(19.4624497);
+        return l;
+    }
+
+    public Location getAgrafOfficeLocation(){
+        Location l = new Location("afraf");
+        l.setLatitude(51.7590930);
+        l.setLongitude(19.449537);
+        return l;
+    }
+    public Location getMediaOfficeLocation(){
+        Location l = new Location("orion");
+        l.setLatitude(51.7566488);
+        l.setLongitude(19.4435929);
+        return l;
+    }
+}
