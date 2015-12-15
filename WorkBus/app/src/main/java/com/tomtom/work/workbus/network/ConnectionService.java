5139cddc5c560edad546769ccfbaac1f02a62a3c
Mariusz Saramak <mariusz.saramak@tomtom.com>
2015-12-15 15:55:26 +0100

    ConnectionService created

---
+++ WorkBus/app/src/main/java/com/tomtom/work/workbus/network/ConnectionService.java
@@ -0,0 +1,31 @@
+package com.tomtom.work.workbus.network;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ConnectionService extends Service {
+
+    public ConnectionService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
