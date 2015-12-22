0ce6880889ca2da765ccef05e02e78f3d453966e
Mariusz Saramak <mariusz.saramak@tomtom.com>
2015-12-22 10:56:30 +0100

    Bind buttons

--- WorkBus/app/src/main/java/com/tomtom/work/workbus/MainActivityFragment.java
+++ WorkBus/app/src/main/java/com/tomtom/work/workbus/MainActivityFragment.java
@@ -1,3 +1,4 @@
+
 package com.tomtom.work.workbus;
 
 import android.support.v4.app.Fragment;
@@ -5,6 +6,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,9 +21,27 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+    @Bind(R.id.current_location_tv)
+    TextView currentLocationTextView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, rootView);
+
+        return rootView;
+    }
+
+    @OnClick(R.id.media_button) void onMediaOfficeClick(){
+
+    }
+
+    @OnClick(R.id.orion_button) void onOrionOfficeClick(){
+
+    }
+
+    @OnClick(R.id.agraf_button) void onAgrafOfficeClick(){
+
     }
 }
